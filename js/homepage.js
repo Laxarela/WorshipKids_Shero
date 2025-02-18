f192f232edf30174bd61746c7580d901eae0a3d0
Laxarela <lelakreka@gmail.com>
2025-02-18 23:00:53 +0100

    fixes

--- js/homepage.js
+++ js/homepage.js
@@ -21,28 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const menuIcon = document.querySelector(".menu-icon");
-//   const closeIcon = document.querySelector(".close-icon");
-//   const navLinks = document.querySelector(".nav-links");
-//   const body = document.body;
-
-//   menuIcon.addEventListener("click", function () {
-//       navLinks.classList.add("active");
-//       menuIcon.classList.add("hide");
-//       closeIcon.classList.add("show");
-//       body.classList.add("no-scroll"); // Disable scrolling
-//   });
-
-//   closeIcon.addEventListener("click", function () {
-//       navLinks.classList.remove("active");
-//       menuIcon.classList.remove("hide");
-//       closeIcon.classList.remove("show");
-//       body.classList.remove("no-scroll"); // Enable scrolling again
-//   });
-// });
-
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
   // homepage banner slider
@@ -72,23 +50,17 @@ function showSlides(n) {
 
 const scrollers = document.querySelectorAll(".scroller");
 
-// If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
 function addAnimation() {
   scrollers.forEach((scroller) => {
-    // add data-animated="true" to every `.scroller` on the page
     scroller.setAttribute("data-animated", true);
 
-    // Make an array from the elements within `.scroller-inner`
     const scrollerInner = scroller.querySelector(".scroller__inner");
     const scrollerContent = Array.from(scrollerInner.children);
-
-    // For each item in the array, clone it
-    // add aria-hidden to it
-    // add it into the `.scroller-inner`
+    
     scrollerContent.forEach((item) => {
       const duplicatedItem = item.cloneNode(true);
       duplicatedItem.setAttribute("aria-hidden", true);
@@ -96,3 +68,5 @@ function addAnimation() {
     });
   });
 }
+
+
